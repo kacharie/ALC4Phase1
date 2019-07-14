355728be97fc7108f58c3d32cd5b0bef6513791b
kacharie <charity@safeboda.com>
2019-07-14 22:27:32 +0300

    Modified

--- app/src/main/java/com/example/chari/alc4phase1/ActivityB.java
+++ app/src/main/java/com/example/chari/alc4phase1/ActivityB.java
@@ -1,7 +1,10 @@
 package com.example.chari.alc4phase1;
 
+import android.content.Intent;
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -11,13 +14,22 @@ public class ActivityB extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+//Action bar title
+        getSupportActionBar().setTitle("About ALC");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        WebView webView =  findViewById(R.id.web_view);
+//load the url
+
+
+        WebView webView = findViewById(R.id.web_view);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl("http://andela.com/alc/");
+        webSettings.setAllowContentAccess(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        webView.loadUrl("https://andela.com/alc/");
     }
-
-
-
 }
+
+
+
+
--- app/src/main/java/com/example/chari/alc4phase1/ActivityC.java
+++ app/src/main/java/com/example/chari/alc4phase1/ActivityC.java
@@ -1,5 +1,6 @@
 package com.example.chari.alc4phase1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,13 @@ public class ActivityC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c);
+
+        getSupportActionBar().setTitle("My Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
 
 
     }
